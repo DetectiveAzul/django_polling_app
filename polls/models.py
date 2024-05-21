dfc914821ee1836a037dd2efb350e3dd8f93f52b
Cian <cian.lopez@travelperk.com>
2024-05-21 14:57:44 +0100

    Customise admin view to be able to create question with choices

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,8 @@ import datetime
 from django.utils import timezone
 from django.db import models
 
+from polls import admin
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,6 +12,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now >= self.pub_date >= now - datetime.timedelta(days = 1) 
