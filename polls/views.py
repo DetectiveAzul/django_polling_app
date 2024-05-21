8d1f9c923735c9ca09cd4313b86d4da6ba852595
Cian <cian.lopez@travelperk.com>
2024-05-21 13:23:43 +0100

    Add some placeholder endpoints for polling operations

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("hello, world. You're at the polls index");
+
+def detail(request, question_id):
+    response = "You're looking at the details of question %s."
+    return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    response = "You're voting on question %s."
+    return HttpResponse(response % question_id)
